29170f2c53773b98b179b335013380ce3066d526
Thomas Bernard <thomas.t.bernard@gmail.com>
2015-02-23 23:42:19 +0100

    Retour de la méthode search dans la classe Node plutot que BPlusTree

--- src/btree/Node.java
+++ src/btree/Node.java
@@ -1,5 +1,7 @@
 package btree;
 
+import java.util.Arrays;
+
 /**
  * Classe Node
  * 
@@ -130,8 +132,37 @@ public class Node {
 		
 	}
 	
-	public void search(int i){
+	public Node search(int k, Node node) {
+
+		/* Si k est trouvé dans les valeurs de node, i vaut son index dans le tableau */
+		/* Si k n'est pas trouvé, i vaut -1 */
+		int i = Arrays.asList(node.getValues()).indexOf(k);
 		
+		/* Si le noeud est une feuille du B+-Tree */
+		if (node.isLeaf()) {
+			
+			if (i != -1) {
+				/* Le noeud est une feuille et la valeur cherchée appartient aux valeurs de ce noeud */
+				return node;
+			} else {
+				/* Le noeud est une feuille mais la valeur cherchée n'appartient pas aux valeurs de ce noeud */
+				return null;
+			}
+			
+		}
+		
+		/* Si au contraire le noeud est un noeud interne du B+-Tree */
+		else {
+
+			if (Arrays.asList(node.getValues()).contains(k)) {
+				/* Le noeud est un noeud interne et la valeur cherchée appartient aux valeurs de ce noeud */
+				return null;
+			} else {
+				/* Le noeud est un noeud interne mais la valeur cherchée n'appartient pas aux valeurs de ce noeud */
+				return null;
+			}
+			
+		}
 	}
 	
 }
