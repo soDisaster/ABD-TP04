86fff3724578aa1d69157cc4c7c451a14a25fc8b
Thomas Bernard <thomas.t.bernard@gmail.com>
2015-02-23 16:31:53 +0100

    Ajout d'une classe Main

---
+++ src/btree/Main.java
@@ -0,0 +1,12 @@
+package btree;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Node node = new Node(true);
+		System.out.println(node.isLeaf());
+		node.getChildren()[2] = new Node(false);
+		System.out.println(node.isLeaf());
+	}
+
+}
