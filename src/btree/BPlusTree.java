33e180974a15eb0f8b377f1399deb674ff19d299
Thomas Bernard <thomas.t.bernard@gmail.com>
2015-02-23 23:41:54 +0100

    Modification de la méthode search

--- src/btree/BPlusTree.java
+++ src/btree/BPlusTree.java
@@ -4,40 +4,34 @@ import java.util.Arrays;
 
 public class BPlusTree {
 
-	public void search(int k, Node node) {
+	public Node search(int k, Node node) {
+
+		/* Si k est trouvé dans les valeurs de node, i vaut son index dans le tableau */
+		/* Si k n'est pas trouvé, i vaut -1 */
+		int i = Arrays.asList(node.getValues()).indexOf(k);
+		
+		/* Si le noeud est une feuille du B+-Tree */
 		if (node.isLeaf()) {
 			
-			//
-			// En fait, on peut pas se contenter de juste vérifier que la valeur est présente, car on doit récupérer
-			// l'indice i de cette valeur dans le noeud
-			//
-			if (Arrays.asList(node.getValues()).contains(k)) {
+			if (i != -1) {
 				/* Le noeud est une feuille et la valeur cherchée appartient aux valeurs de ce noeud */
-				
-				//
-				// TODO : Le diapo dit de retourner Pi, c'est à dire le pointeur qui se trouve juste
-				// avant la valeur qu'on vient de trouver, mais ça me parait bizarre.
-				// En attendant, je me contente de retourner le noeud
-				//
-				//return node;
-				//
+				return node;
 			} else {
 				/* Le noeud est une feuille mais la valeur cherchée n'appartient pas aux valeurs de ce noeud */
-				
+				return null;
 			}
 			
-		} else {
+		}
+		
+		/* Si au contraire le noeud est un noeud interne du B+-Tree */
+		else {
 
-			//
-			// En fait, on peut pas se contenter de juste vérifier que la valeur est présente, car on doit récupérer
-			// l'indice i de cette valeur dans le noeud
-			//
 			if (Arrays.asList(node.getValues()).contains(k)) {
 				/* Le noeud est un noeud interne et la valeur cherchée appartient aux valeurs de ce noeud */
-				
+				return null;
 			} else {
 				/* Le noeud est un noeud interne mais la valeur cherchée n'appartient pas aux valeurs de ce noeud */
-				
+				return null;
 			}
 			
 		}
