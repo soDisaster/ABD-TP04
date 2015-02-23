61c35e3ca30ccfa356f3ddc0b9b48c9c9f6bd9e3
Thomas Bernard <thomas.t.bernard@gmail.com>
2015-02-23 16:04:24 +0100

    Documentation

--- src/btree/Node.java
+++ src/btree/Node.java
@@ -1,21 +1,72 @@
 package btree;
 
+/**
+ * Classe Node
+ * 
+ * Node est un noeud (interne ou feuille) d'un B+-Tree
+ * 
+ * @author Anne-Sophie Saint-Omer & Thomas Bernard
+ */
 public class Node {
 	
+	/* ========= */
+	/* ATTRIBUTS */
+	/* ========= */
+	
+	/**
+	 * Pointeurs sur les noeuds fils du noeud courant
+	 */
 	protected Node[] children;
+	
+	/**
+	 * Valeurs des clés du noeud courant
+	 */
 	protected Integer [] values;
+	
+	/**
+	 * Pointeur sur le noeud suivant
+	 */
 	Node next;
+	
+	/**
+	 * Pointeur sur le noeud précédent
+	 */
 	Node previous;
+	
+	/**
+	 * Racine du B+-Tree
+	 */
 	boolean root;
 	
+	/* ============ */
+	/* CONSTRUCTEUR */
+	/* ============ */
+	
+	/**
+	 * Constructeur de la classe Node
+	 * 
+	 * @param c
+	 * 			Noeuds fils du noeud courant
+	 * @param v
+	 * 			Valeurs des clés du noeud courant
+	 * @param n
+	 * 			Pointeur sur le noeud suivant
+	 * @param p
+	 * 			Pointeur sur le noeud précédent
+	 * @param r
+	 * 			Racine du B+-Tree
+	 */
 	public Node(Node[] c, Integer[] v, Node n, Node p, boolean r) {
-		
 		this.children = c;
 		this.values = v;
 		this.next = n;
 		this.previous = p;
 		this.root = r;
 	}
+	
+	/* ======== */
+	/* METHODES */
+	/* ======== */
 	
 	public Node[] getChildren() {
 		return this.children;
