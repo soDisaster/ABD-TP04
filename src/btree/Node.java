489a2823dd67b4cd3faf10ec847ee7ab36874673
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-02-23 22:10:48 +0100

    Méthodes insertion, suppression, recherche

--- src/btree/Node.java
+++ src/btree/Node.java
@@ -2,14 +2,16 @@ package btree;
 
 public class Node {
 	
+	protected int indice;
 	protected Node[] children;
 	protected Integer [] values;
 	Node next;
 	Node previous;
 	boolean root;
 	
-	public Node(Node[] c, Integer[] v, Node n, Node p, boolean r) {
+	public Node(Node[] c, Integer[] v, Node n, Node p, boolean r){
 		
+		this.indice = 4;
 		this.children = c;
 		this.values = v;
 		this.next = n;
@@ -17,25 +19,40 @@ public class Node {
 		this.root = r;
 	}
 	
-	public Node[] getChildren() {
+	public Node[] getChildren(){
 		return this.children;
 	}
 	
-	public Integer[] getValues() {
+	public Integer[] getValues(){
 		return this.values;
 	}
 	
-	public Node getNext() {
+	public Node getNext(){
 		return this.next;
 	}
 	
-	public Node getPrevious() {
+	public Node getPrevious(){
 		return this.previous;
 	}
 	
-	public boolean isRoot() {
+	public boolean isRoot(){
 		return this.root;
 	}
+	
+	public void insert(int i){
+	}
+	
+	public void remove(int i){
+		
+	}
+	
+	public void search(int i){
+		
+	}
+	
+	
+	
+	
 
 
 }
